50dba8c04e2cb7c4067643d62beb7a328e200344
Aiken Tine Ahac <ahac.aiken@gmail.com>
2020-09-15 22:31:55 +0200

    Auth system finished

--- config/db.js
+++ config/db.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 // Replace this with your MONGOURI.
 const {
     MONGOURI
-} = require("../mongodbCreds.json");
+} = require("../config.json");
 
 const InitiateMongoServer = async () => {
   try {
     await mongoose.connect(MONGOURI, {
       useNewUrlParser: true
     });
-    console.log("Connected to DB !!");
+    console.log("Connected to Database!");
   } catch (e) {
     console.log(e);
     throw e;
