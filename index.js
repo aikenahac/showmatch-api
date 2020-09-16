b03f6a573a890d23d47675edac94d0ca78ddc551
Aiken Tine Ahac <ahac.aiken@gmail.com>
2020-09-16 09:50:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8000;
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "API Working" });
+  res.json({ message: "ShowMatch API Working" });
 });
 
 
